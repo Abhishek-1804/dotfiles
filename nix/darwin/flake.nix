78344b8b5e7cb48bbaed8c04c63d90e396a7e355
Abhishek <abhidp55@gmail.com>
2024-10-25 13:04:51 -0700

    making setup more modular

--- nix/darwin/flake.nix
+++ nix/darwin/flake.nix
@@ -13,65 +13,39 @@
   };
 
   outputs = inputs@{ self, nix-darwin, nixpkgs, nix-homebrew, home-manager}:
-  let
-    configuration = { pkgs, config, ... }: {
+    let
+      configuration = { pkgs, config, ... }: {
 
-      # Allowing proprietary software download
-      nixpkgs.config.allowUnfree = true;
+        # Allowing proprietary software download
+        nixpkgs.config.allowUnfree = true;
 
-      # List packages installed in system profile. To search by name, run:
-      # $ nix-env -qaP | grep wget
-      environment.systemPackages =
-        [
-            # CLI tools
-            pkgs.mkalias
-            pkgs.bat
-            pkgs.bun
-            pkgs.cmatrix
-            pkgs.fastfetch
-            pkgs.fd
-            pkgs.fdupes
-            pkgs.ffmpeg
-            pkgs.fzf
-            pkgs.hping
-            pkgs.lazygit
-            pkgs.deno
-            pkgs.neovim
-            pkgs.nginx
-            pkgs.nmap
-            pkgs.ollama
-            pkgs.lynx
-            pkgs.jdk
-            pkgs.rustc
-            pkgs.openssl
-            pkgs.transmission_4
-            pkgs.pnpm
-            pkgs.mas
-            pkgs.postgresql_14
-            pkgs.ripgrep
-            pkgs.sl
-            pkgs.stow
-            pkgs.nodejs_22
-            pkgs.starship
-            pkgs.tlrc
-            pkgs.tmux
-            pkgs.tree
-            pkgs.yt-dlp
-            pkgs.yarn
+        # List packages installed in system profile. To search by name, run:
+        # $ nix-env -qaP | grep wget
+        environment.systemPackages = pkgs.callPackage ../shared/packages.nix {} ++ [
+          # CLI tools
+          pkgs.mkalias
+          pkgs.hping
+          pkgs.lynx
+          pkgs.rustc
+          pkgs.openssl
+          pkgs.yt-dlp
+          pkgs.transmission_4
+          pkgs.mas
 
-            # Gui applications
-            pkgs.iterm2
-            pkgs.arc-browser
-            pkgs.raycast
-            pkgs.postman
-            pkgs.zoom-us
-            pkgs.pgadmin4
-            pkgs.vscode
+          # GUI applications
+          pkgs.iterm2
+          pkgs.zoom-us
+          pkgs.arc-browser
+          pkgs.raycast
+          pkgs.postman
+          pkgs.pgadmin4
+          pkgs.vscode
         ];
 
-      # Homebrew stuff
-      homebrew = {
+        # Homebrew stuff
+        homebrew = {
           enable = true;
+
           brews = [
             "btop"
             "gnu-sed"
@@ -79,17 +53,20 @@
             "luarocks"
             "watch"
           ];
+
           casks = [
-              "docker"
-              "chatgpt"
-              "google-drive"
-              "notion"
-              "surfshark"
-              "whatsapp"
+            "docker"
+            "chatgpt"
+            "google-drive"
+            "notion"
+            "surfshark"
+            "whatsapp"
           ];
+
           masApps = {
             "Amphetamine" = 937984704;
           };
+
           onActivation.cleanup = "zap";
           onActivation.autoUpdate = true;
           onActivation.upgrade = true;
@@ -97,18 +74,22 @@
 
         # Font config
         fonts.packages = [
-          (pkgs.nerdfonts.override {fonts = ["JetBrainsMono"];})
+          (pkgs.nerdfonts.override {
+            fonts = [
+              "JetBrainsMono"
+            ];
+          })
         ];
 
-      # To find applications installed from nixpkgs in spotlight search
-      system.activationScripts.applications.text = let
-        env = pkgs.buildEnv {
-          name = "system-applications";
-          paths = config.environment.systemPackages;
-          pathsToLink = "/Applications";
-        };
-      in
-        pkgs.lib.mkForce ''
+        # To find applications installed from nixpkgs in spotlight search
+        system.activationScripts.applications.text = let
+          env = pkgs.buildEnv {
+            name = "system-applications";
+            paths = config.environment.systemPackages;
+            pathsToLink = "/Applications";
+          };
+        in
+          pkgs.lib.mkForce ''
         # Set up applications.
         echo "setting up /Applications..." >&2
         rm -rf /Applications/Nix\ Apps
@@ -117,12 +98,12 @@
         while read src; do
           app_name=$(basename "$src")
           echo "copying $src" >&2
-          ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
+            ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
         done
-      '';
+          '';
 
-      # Mac system default settings
-      system.defaults = {
+        # Mac system default settings
+        system.defaults = {
           dock.autohide = true;
           dock.persistent-apps = [
             "System/Applications/Mail.app"
@@ -156,36 +137,36 @@
           NSGlobalDomain.KeyRepeat = 2;
         };
 
-      # Auto upgrade nix package and the daemon service.
-      services.nix-daemon.enable = true;
-      # nix.package = pkgs.nix;
+        # Auto upgrade nix package and the daemon service.
+        services.nix-daemon.enable = true;
+        # nix.package = pkgs.nix;
 
-      # Necessary for using flakes on this system.
-      nix.settings.experimental-features = "nix-command flakes";
+        # Necessary for using flakes on this system.
+        nix.settings.experimental-features = "nix-command flakes";
 
-      # Create /etc/zshrc that loads the nix-darwin environment.
-      programs.zsh.enable = true;  # default shell on catalina
-      # programs.fish.enable = true;
+        # Create /etc/zshrc that loads the nix-darwin environment.
+        programs.zsh.enable = true;  # default shell on catalina
+        # programs.fish.enable = true;
 
-      # Set Git commit hash for darwin-version.
-      system.configurationRevision = self.rev or self.dirtyRev or null;
+        # Set Git commit hash for darwin-version.
+        system.configurationRevision = self.rev or self.dirtyRev or null;
 
-      # Used for backwards compatibility, please read the changelog before changing.
-      # $ darwin-rebuild changelog
-      system.stateVersion = 5;
+        # Used for backwards compatibility, please read the changelog before changing.
+        # $ darwin-rebuild changelog
+        system.stateVersion = 5;
 
-      # The platform the configuration will be used on.
-      nixpkgs.hostPlatform = "aarch64-darwin";
+        # The platform the configuration will be used on.
+        nixpkgs.hostPlatform = "aarch64-darwin";
 
-      # Users
-      users.users.abhishekdeshpande.home = "/Users/abhishekdeshpande";
-    };
-  in
-  {
-    # Build darwin flake using:
-    # $ darwin-rebuild build --flake .#Abhisheks-MacBook-Air
-    darwinConfigurations."Abhisheks-MacBook-Air" = nix-darwin.lib.darwinSystem {
-      modules = [ 
+        # Users
+        users.users.abhishekdeshpande.home = "/Users/abhishekdeshpande";
+      };
+    in
+      {
+      # Build darwin flake using:
+      # $ darwin-rebuild build --flake .#Abhisheks-MacBook-Air
+      darwinConfigurations."Abhisheks-MacBook-Air" = nix-darwin.lib.darwinSystem {
+        modules = [ 
           configuration 
           nix-homebrew.darwinModules.nix-homebrew
           {
@@ -202,9 +183,9 @@
             home-manager.users.abhishekdeshpande = import ./home.nix;
           }
         ];
-    };
+      };
 
-    # Expose the package set, including overlays, for convenience.
-    darwinPackages = self.darwinConfigurations."Abhisheks-MacBook-Air".pkgs;
-  };
+      # Expose the package set, including overlays, for convenience.
+      darwinPackages = self.darwinConfigurations."Abhisheks-MacBook-Air".pkgs;
+    };
 }
---
+++ nix/shared/packages.nix
@@ -0,0 +1,33 @@
+{ pkgs, ... }:
+
+[
+  # CLI tools
+  pkgs.bat
+  pkgs.bun
+  pkgs.cmatrix
+  pkgs.fastfetch
+  pkgs.fd
+  pkgs.fdupes
+  pkgs.ffmpeg
+  pkgs.fzf
+  pkgs.lazygit
+  pkgs.deno
+  pkgs.neovim
+  pkgs.nginx
+  pkgs.nmap
+  pkgs.ollama
+  pkgs.jdk
+  pkgs.pnpm
+  pkgs.ripgrep
+  pkgs.postgresql_14
+  pkgs.sl
+  pkgs.stow
+  pkgs.nodejs_22
+  pkgs.starship
+  pkgs.tlrc
+  pkgs.tmux
+  pkgs.tree
+  pkgs.yarn
+
+  # GUI applications
+]
