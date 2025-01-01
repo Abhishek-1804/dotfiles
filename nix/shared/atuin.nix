cd637741a1704be6c37f337151c6976aab2766e6
Abhishek <abhidp55@gmail.com>
2024-12-31 18:50:23 -0700

    add pkg: atuin, ghostty (common)

--- nix/shared/atuin.nix
+++ nix/shared/atuin.nix
@@ -7,9 +7,10 @@
     style = "auto";
     sync_address = "https://api.atuin.sh";
     sync_frequency = "15m";
-      auto_sync = true;
-    theme = {
-      name = "autumn"; # Specify your preferred theme
-    };
+    auto_sync = true;
+    enter_accept = true;  # Add this line to enable enter to accept
+      theme = {
+        name = "autumn"; # Specify your preferred theme
+      };
   };
 }
