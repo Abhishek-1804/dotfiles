e8cb519adddb6c8fffd70e167c9a06431a922285
Abhishek <abhidp55@gmail.com>
2025-01-15 11:18:23 -0700

    config update: essential packages

---
+++ nix/shared/modules/essential.nix
@@ -0,0 +1,31 @@
+{ pkgs, ... }:
+
+[
+# Programming Languages and Development Tools
+  pkgs.git
+  pkgs.nodejs
+  pkgs.python3
+  pkgs.rustc
+  pkgs.cargo
+  pkgs.go
+
+# Networking and Security Tools
+  pkgs.aria2
+  pkgs.rsync
+
+# CLI Utilities and System Tools
+  pkgs.bat
+  pkgs.fd
+  pkgs.ripgrep
+  pkgs.fzf
+  pkgs.tree
+  pkgs.lazygit
+  pkgs.lazydocker
+  pkgs.tlrc
+  pkgs.yazi
+
+# Media and Content Tools
+
+# Specialized Tools
+  ]
+
