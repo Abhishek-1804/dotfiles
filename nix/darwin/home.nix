1e2ada945133232673391097998463353cd809b2
Abhishek <abhidp55@gmail.com>
2025-01-15 11:56:57 -0700

    config update: added force to symlinks

--- nix/darwin/home.nix
+++ nix/darwin/home.nix
@@ -16,34 +16,60 @@
     echo "Starting LazyVim installation..."
     NVIM_CONFIG="${config.xdg.configHome}/nvim"
 
-    # Check if the ~/.config/nvim directory does NOT exist
+# Check if the ~/.config/nvim directory does NOT exist
     if [ ! -d "$NVIM_CONFIG" ]; then
       echo "No nvim directory found. Setting up LazyVim..."
 
-      rm -rf "$NVIM_CONFIG"
-      mkdir -p "$NVIM_CONFIG"
-      ${pkgs.git}/bin/git clone https://github.com/LazyVim/starter "$NVIM_CONFIG"
-      rm -rf "$NVIM_CONFIG/.git"
+        rm -rf "$NVIM_CONFIG"
+        mkdir -p "$NVIM_CONFIG"
+        ${pkgs.git}/bin/git clone https://github.com/LazyVim/starter "$NVIM_CONFIG"
+        rm -rf "$NVIM_CONFIG/.git"
 
-      echo "LazyVim installed successfully!"
+        echo "LazyVim installed successfully!"
     else
       echo "nvim directory already exists, skipping LazyVim installation"
-    fi
-  '';
+        fi
+        '';
 
 # Manage files
   home.file = {
-    ".config/nvim/lua/config/autocmds.lua".source = ../../.config/nvim/lua/config/autocmds.lua;
-    ".config/nvim/lua/config/keymaps.lua".source = ../../.config/nvim/lua/config/keymaps.lua;
-    ".config/nvim/lua/config/lazy.lua".source = ../../.config/nvim/lua/config/lazy.lua;
-    ".config/nvim/lua/config/options.lua".source = ../../.config/nvim/lua/config/options.lua;
-    ".config/nvim/lua/plugins/colorscheme.lua".source = ../../.config/nvim/lua/plugins/colorscheme.lua;
-    ".config/nvim/lua/plugins/genAI.lua".source = ../../.config/nvim/lua/plugins/genAI.lua;
-    ".config/nvim/lua/plugins/leetcode.lua".source = ../../.config/nvim/lua/plugins/leetcode.lua;
-    ".config/nvim/lua/plugins/vimTmuxNav.lua".source = ../../.config/nvim/lua/plugins/vimTmuxNav.lua;
-    ".config/ghostty/config".source = ../../.config/ghostty/config;
+    ".config/nvim/lua/config/autocmds.lua" = {
+      source = ../../.config/nvim/lua/config/autocmds.lua;
+      force = true;
+    };
+    ".config/nvim/lua/config/keymaps.lua" = {
+      source = ../../.config/nvim/lua/config/keymaps.lua;
+      force = true;
+    };
+    ".config/nvim/lua/config/lazy.lua" = {
+      source = ../../.config/nvim/lua/config/lazy.lua;
+      force = true;
+    };
+    ".config/nvim/lua/config/options.lua" = {
+      source = ../../.config/nvim/lua/config/options.lua;
+      force = true;
+    };
+    ".config/nvim/lua/plugins/colorscheme.lua" = {
+      source = ../../.config/nvim/lua/plugins/colorscheme.lua;
+      force = true;
+    };
+    ".config/nvim/lua/plugins/genAI.lua" = {
+      source = ../../.config/nvim/lua/plugins/genAI.lua;
+      force = true;
+    };
+    ".config/nvim/lua/plugins/leetcode.lua" = {
+      source = ../../.config/nvim/lua/plugins/leetcode.lua;
+      force = true;
+    };
+    ".config/nvim/lua/plugins/vimTmuxNav.lua" = {
+      source = ../../.config/nvim/lua/plugins/vimTmuxNav.lua;
+      force = true;
+    };
+    ".config/ghostty/config" = {
+      source = ../../.config/ghostty/config;
+      force = true;
+    };
   };
-
 
   programs = {
     neovim = (import ../shared/neovim.nix { inherit config pkgs; });
