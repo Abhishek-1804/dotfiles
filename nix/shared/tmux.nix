0da762d0904db39d9e0805e7af6f0981ed92fd74
Abhishek <abhidp55@gmail.com>
2025-03-11 17:00:44 +0530

    config update: font, touchId, tmux-plugin syntax (darwin, common)

--- nix/shared/tmux.nix
+++ nix/shared/tmux.nix
@@ -1,41 +1,42 @@
 { pkgs, ... }:
 
 let
-# Define the catppuccin Tmux theme plugin
-catppuccin = pkgs.tmuxPlugins.mkTmuxPlugin {
-  pluginName = "catppuccin";
-  version = "unstable-2024-05-15";
-  src = pkgs.fetchFromGitHub {
-    owner = "catppuccin";
-    repo = "tmux";
-    rev = "697087f593dae0163e01becf483b192894e69e33";
-    sha256 = "sha256-EHinWa6Zbpumu+ciwcMo6JIIvYFfWWEKH1lwfyZUNTo=";
+  # Define the catppuccin Tmux theme plugin
+  catppuccin = pkgs.tmuxPlugins.mkTmuxPlugin {
+    pluginName = "catppuccin";
+    version = "unstable-2024-05-15";
+    src = pkgs.fetchFromGitHub {
+      owner = "catppuccin";
+      repo = "tmux";
+      rev = "697087f593dae0163e01becf483b192894e69e33";
+      sha256 = "sha256-EHinWa6Zbpumu+ciwcMo6JIIvYFfWWEKH1lwfyZUNTo=";
+    };
   };
-};
 
-# Define the tmux-resurrect plugin
-resurrect = pkgs.tmuxPlugins.mkTmuxPlugin {
-  pluginName = "resurrect";
-  version = "unstable-2022-05-01";
-  src = pkgs.fetchFromGitHub {
-    owner = "tmux-plugins";
-    repo = "tmux-resurrect";
-    rev = "ca6468e2deef11efadfe3a62832ae67742505432";
-    sha256 = "0d7jg5dy4jq64679rf2zqmqbvgiqvpcj5jxfljk7d7y86dnqhj3n";
+  # Define the tmux-resurrect plugin
+  resurrect = pkgs.tmuxPlugins.mkTmuxPlugin {
+    pluginName = "resurrect";
+    version = "unstable-2022-05-01";
+    src = pkgs.fetchFromGitHub {
+      owner = "tmux-plugins";
+      repo = "tmux-resurrect";
+      rev = "ca6468e2deef11efadfe3a62832ae67742505432";
+      hash = "sha256-wl9/5XvFq+AjV8CwYgIZjPOE0/kIuEYBNQqNDidjNFo=";
+      fetchSubmodules = true;
+    };
   };
-};
 
-# Detect or install `zsh`
-zshPath = if pkgs.stdenv.isDarwin then "/bin/zsh" else "${pkgs.zsh}/bin/zsh";
+  # Detect or install `zsh`
+  zshPath = if pkgs.stdenv.isDarwin then "/bin/zsh" else "${pkgs.zsh}/bin/zsh";
 
 in
-{
+  {
   enable = true;
 
   plugins = with pkgs.tmuxPlugins; [
     resurrect
-      catppuccin
-      vim-tmux-navigator
+    catppuccin
+    vim-tmux-navigator
   ];
 
   extraConfig = ''
@@ -43,7 +44,7 @@ in
 
     set -g mouse on
 
-# Add these lines
+    # Add these lines
     set-option -sa terminal-features ',xterm-256color:RGB'
     set -g default-terminal "xterm-256color"
     set -g @catppuccin_flavour 'mocha' # or your preferred flavor
@@ -75,5 +76,5 @@ in
     set -g @catppuccin_status_connect_separator "no"
 
     set -g @catppuccin_directory_text "#{pane_current_path}"
-    '';
+  '';
 }
