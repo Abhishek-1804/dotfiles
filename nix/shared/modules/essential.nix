400dfa4f4bb68e4894f9c3d32aff7400876f9d98
Abhishek <abhidp55@gmail.com>
2025-05-24 14:49:47 -0700

    config update: restructured modules (common)
    merged essential packages in the main one.
    packages added: duf, eza, asciinema, fabric-ai

--- nix/shared/modules/essential.nix
+++ nix/shared/modules/essential.nix
@@ -1,31 +1,36 @@
 { pkgs, ... }:
 
 [
-# Programming Languages and Development Tools
+  # Programming Languages and Development Tools
   pkgs.git
   pkgs.nodejs
   pkgs.python3
   pkgs.rustc
   pkgs.cargo
   pkgs.go
+  pkgs.gcc
+  pkgs.cmake
+  ## cross-platform asynchronous I/O & event loop library
+  pkgs.libuv
 
-# Networking and Security Tools
+  # Networking and Security Tools
+  ## multi-protocol, high-speed command-line downloader
   pkgs.aria2
   pkgs.rsync
 
-# CLI Utilities and System Tools
+  # CLI Utilities and System Tools
   pkgs.bat
+  ## simple, fast, user-friendly alternative to `find`
   pkgs.fd
+  ## friendly, colored output
+  pkgs.duf
   pkgs.ripgrep
   pkgs.fzf
-  pkgs.tree
+  # modern replacement for `ls` with Git & icons support
+  pkgs.eza
   pkgs.lazygit
   pkgs.lazydocker
   pkgs.tlrc
   pkgs.yazi
 
-# Media and Content Tools
-
-# Specialized Tools
-  ]
-
+]
