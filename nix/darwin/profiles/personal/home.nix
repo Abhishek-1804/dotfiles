2449ee7e92e03f315c86711fe2f3a20105cfa2c4
Abhishek <abhidp55@gmail.com>
2025-05-25 15:41:44 -0700

    config update: modularised everything abit
    minor changes

--- nix/darwin/profiles/personal/home.nix
+++ nix/darwin/profiles/personal/home.nix
@@ -14,50 +14,50 @@
 # Manage files
   home.file = {
     ".config/nvim/lua/config/autocmds.lua" = {
-      source = ../../.config/nvim/lua/config/autocmds.lua;
+      source = ../../../../.config/nvim/lua/config/autocmds.lua;
       force = true;
     };
     ".config/nvim/lua/config/keymaps.lua" = {
-      source = ../../.config/nvim/lua/config/keymaps.lua;
+      source = ../../../../.config/nvim/lua/config/keymaps.lua;
       force = true;
     };
     ".config/nvim/lua/config/lazy.lua" = {
-      source = ../../.config/nvim/lua/config/lazy.lua;
+      source = ../../../../.config/nvim/lua/config/lazy.lua;
       force = true;
     };
     ".config/nvim/lua/config/options.lua" = {
-      source = ../../.config/nvim/lua/config/options.lua;
+      source = ../../../../.config/nvim/lua/config/options.lua;
       force = true;
     };
     ".config/nvim/lua/plugins/colorscheme.lua" = {
-      source = ../../.config/nvim/lua/plugins/colorscheme.lua;
+      source = ../../../../.config/nvim/lua/plugins/colorscheme.lua;
       force = true;
     };
     ".config/nvim/lua/plugins/genAI.lua" = {
-      source = ../../.config/nvim/lua/plugins/genAI.lua;
+      source = ../../../../.config/nvim/lua/plugins/genAI.lua;
       force = true;
     };
     ".config/nvim/lua/plugins/leetcode.lua" = {
-      source = ../../.config/nvim/lua/plugins/leetcode.lua;
+      source = ../../../../.config/nvim/lua/plugins/leetcode.lua;
       force = true;
     };
     ".config/nvim/lua/plugins/vimTmuxNav.lua" = {
-      source = ../../.config/nvim/lua/plugins/vimTmuxNav.lua;
+      source = ../../../../.config/nvim/lua/plugins/vimTmuxNav.lua;
       force = true;
     };
     ".config/ghostty/config" = {
-      source = ../../.config/ghostty/config;
+      source = ../../../../.config/ghostty/config;
       force = true;
     };
   };
 
   programs = {
-    neovim = (import ../shared/neovim.nix { inherit config pkgs; });
-    zsh = (import ../shared/zsh.nix { inherit config pkgs; });
-    vim = (import ../shared/vim.nix { inherit config pkgs; });
-    tmux = (import ../shared/tmux.nix { inherit config pkgs; });
-    starship = (import ../shared/starship.nix { inherit config pkgs; });
-    atuin = (import ../shared/atuin.nix { inherit config pkgs; });
+    neovim = (import ../../../shared/neovim.nix { inherit config pkgs; });
+    zsh = (import ../../../shared/zsh.nix { inherit config pkgs; });
+    vim = (import ../../../shared/vim.nix { inherit config pkgs; });
+    tmux = (import ../../../shared/tmux.nix { inherit config pkgs; });
+    starship = (import ../../../shared/starship.nix { inherit config pkgs; });
+    atuin = (import ../../../shared/atuin.nix { inherit config pkgs; });
   };
 
 # Enable Home Manager to manage itself
